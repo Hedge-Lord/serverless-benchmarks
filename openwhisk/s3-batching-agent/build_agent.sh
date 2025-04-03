8d740581b4a12086f978ff39bba40e3e8a281387
Hedge-Lord <georgezyst@gmail.com>
2025-04-03 00:02:36 -0700

    back to localhost deploying on every dnoe

--- openwhisk/s3-batching-agent/build_agent.sh
+++ openwhisk/s3-batching-agent/build_agent.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e  # 
 
-NODE_IP=$(hostname -I | awk '{print $1}')
-echo "Current node IP: $NODE_IP"
-
 echo "Building Docker image with no cache..."
 docker build --no-cache -t s3-batching-agent:latest .
 
-echo "Tagging image as $NODE_IP:5000/s3-batching-agent:latest..."
-docker tag s3-batching-agent:latest $NODE_IP:5000/s3-batching-agent:latest
+echo "Tagging image as localhost:5000/s3-batching-agent:latest..."
+docker tag s3-batching-agent:latest localhost:5000/s3-batching-agent:latest
 
 echo "Pushing image to local registry..."
-docker push $NODE_IP:5000/s3-batching-agent:latest
+docker push localhost:5000/s3-batching-agent:latest
